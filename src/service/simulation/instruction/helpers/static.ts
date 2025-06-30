b07b048788edac3f711229cf175fc5a7544c4595
Morteza Abolghasemi <a.re.morteza@gmail.com>
2025-07-01 01:51:46 +0330

    add gaussian sampling for setup configs

--- src/service/simulation/instruction/helpers/static.ts
+++ src/service/simulation/instruction/helpers/static.ts
@@ -6,6 +6,21 @@ import {
   SubjectView,
 } from "../types";
 
+function sampleGaussian(mean: number, stdDev: number): number {
+  let u = 0,
+    v = 0,
+    s = 0;
+
+  do {
+    u = Math.random() * 2 - 1;
+    v = Math.random() * 2 - 1;
+    s = u * u + v * v;
+  } while (s >= 1 || s === 0);
+
+  const mul = Math.sqrt((-2.0 * Math.log(s)) / s);
+  return mean + u * mul * stdDev;
+}
+
 export const getVerticalAngle = (
   camera: CameraParameters,
   subjectPosition: THREE.Vector3
@@ -18,59 +33,87 @@ export const getVerticalAngle = (
   return cameraToSubject.angleTo(yAxis);
 };
 
+const ANGLE_STD_DEV = 0.1;
+
 export const getDesiredVerticalAngle = (angle: CameraVerticalAngle): number => {
+  let meanAngle: number;
   switch (angle) {
     case CameraVerticalAngle.Low:
-      return Math.PI * 0.7;
+      meanAngle = Math.PI * 0.7;
+      break;
     case CameraVerticalAngle.Eye:
-      return Math.PI * 0.5;
+      meanAngle = Math.PI * 0.5;
+      break;
     case CameraVerticalAngle.High:
-      return Math.PI * 0.3;
+      meanAngle = Math.PI * 0.3;
+      break;
     case CameraVerticalAngle.Overhead:
-      return Math.PI * 0.1;
+      meanAngle = Math.PI * 0.1;
+      break;
     case CameraVerticalAngle.BirdsEye:
-      return 0;
+      meanAngle = 0;
+      break;
     default:
-      return Math.PI * 0.5;
+      meanAngle = Math.PI * 0.5;
   }
+
+  return sampleGaussian(meanAngle, ANGLE_STD_DEV);
 };
+
+const SHOT_SIZE_DISTANCE_FACTORS = {
+  [ShotSize.ExtremeCloseUp]: 1.5,
+  [ShotSize.CloseUp]: 2.5,
+  [ShotSize.MediumCloseUp]: 3.5,
+  [ShotSize.MediumShot]: 5,
+  [ShotSize.FullShot]: 7,
+  [ShotSize.LongShot]: 10,
+  [ShotSize.VeryLongShot]: 15,
+  [ShotSize.ExtremeLongShot]: 20,
+};
+
+const DISTANCE_STD_DEV_PERCENTAGE = 0.15;
 
 export const getDesiredDistance = (
   shotSize: ShotSize,
   subjectDimensions: { height: number }
 ): number => {
-  const baseDistance = subjectDimensions.height * 2;
-  switch (shotSize) {
-    case ShotSize.CloseUp:
-      return baseDistance * 0.5;
-    case ShotSize.MediumShot:
-      return baseDistance;
-    case ShotSize.LongShot:
-      return baseDistance * 2;
-    default:
-      return baseDistance;
-  }
+  const meanDistance =
+    subjectDimensions.height * (SHOT_SIZE_DISTANCE_FACTORS[shotSize] || 5);
+
+  const stdDev = meanDistance * DISTANCE_STD_DEV_PERCENTAGE;
+
+  return Math.max(0.1, sampleGaussian(meanDistance, stdDev));
 };
 
 export const getDesiredHorizontalAngle = (view: SubjectView): number => {
+  let meanAngle: number;
   switch (view) {
     case SubjectView.Front:
-      return 0;
+      meanAngle = 0;
+      break;
     case SubjectView.Back:
-      return Math.PI;
+      meanAngle = Math.PI;
+      break;
     case SubjectView.Left:
-      return -Math.PI * 0.5;
+      meanAngle = -Math.PI * 0.5;
+      break;
     case SubjectView.Right:
-      return Math.PI * 0.5;
+      meanAngle = Math.PI * 0.5;
+      break;
     case SubjectView.ThreeQuarterFrontLeft:
-      return -Math.PI * 0.25;
+      meanAngle = -Math.PI * 0.25;
+      break;
     case SubjectView.ThreeQuarterFrontRight:
-      return Math.PI * 0.25;
+      meanAngle = Math.PI * 0.25;
+      break;
     case SubjectView.ThreeQuarterBackLeft:
-      return -Math.PI * 0.75;
+      meanAngle = -Math.PI * 0.75;
+      break;
     case SubjectView.ThreeQuarterBackRight:
-      return Math.PI * 0.75;
+      meanAngle = Math.PI * 0.75;
+      break;
     default:
-      return 0;
+      meanAngle = 0;
   }
+  return sampleGaussian(meanAngle, ANGLE_STD_DEV);
 };
